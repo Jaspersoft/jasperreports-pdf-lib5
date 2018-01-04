05df3a093b0fe25ce05e462147cf1e8a3c330547
Teodor Danciu <teodord@users.sourceforge.net>
2018-01-04 15:10:35 +0200

    initial draft

--- src/main/java/com/jaspersoft/jasperreports/export/pdf/JRPdfExporterTagHelper.java
+++ src/main/java/com/jaspersoft/jasperreports/export/pdf/JRPdfExporterTagHelper.java
@@ -1,34 +1,20 @@
 /*
- * JasperReports - Free Java Reporting Library.
- * Copyright (C) 2001 - 2016 TIBCO Software Inc. All rights reserved.
- * http://www.jaspersoft.com
+ * JasperReports Modern PDF Exporter
+ * Copyright © 2005 - 2018 TIBCO Software Inc.
+ * http://www.jaspersoft.com.
  *
- * Unless you have purchased a commercial license agreement from Jaspersoft,
- * the following license terms apply:
- *
- * This program is part of JasperReports.
- *
- * JasperReports is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * JasperReports is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
- * Contributors:
- * Adrian Jackson - iapetus@users.sourceforge.net
- * David Taylor - exodussystems@users.sourceforge.net
- * Lars Kristensen - llk@users.sourceforge.net
- * Ling Li - lonecatz@users.sourceforge.net
- * Martin Clough - mtclough@users.sourceforge.net
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 package com.jaspersoft.jasperreports.export.pdf;
 
